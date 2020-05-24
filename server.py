35fb48c96fbf7a6f4f10be00e7070485ee386f65
inci90 <isaac.keleher@gmail.com>
2020-05-24 10:01:28 +0000

    notification code

---
+++ server.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import os
+from dotenv import load_dotenv
+import logging
+
+# key setup
+load_dotenv()
+KEY = os.getenv("SECRET_KEY")
+
+# logging setup
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# server setup
+app = Flask(__name__)
+app.config['SECRET_KEY'] = KEY
+app.debug = True
+socketio = SocketIO(app)
+
+# route setup
+@app.route('/')
+def index():
+    logger.info('Serving root')
+    return render_template('index.html')
+
+# socket setup
+@socketio.on('event', namespace='/test')
+def response(message):
+    logger.info('respionding to request')
+    emit('server_response',{'data':'hello from server'})
+
+@socketio.on('connect', namespace='test')
+def connect():
+    emit('server_response', {'data': 'Connected to server'})
+
+@socketio.on('disconnect', namespace='/test')
+def disconnect():
+    print('Client disconnected')
+
+if __name__ == '__main__':
+    logger.info('Server Running')
+    socketio.run(app, host='0.0.0.0', port=8080, keyfile='key.pem', certfile='cert.pem')
